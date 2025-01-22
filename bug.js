ae51857dae19ec08beeedf9d5ef4e74238c53c65
BugHunter-X <askzenai@gmail.com>
2025-01-22 21:06:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,10 @@
+function foo(a, b) {
+  if (a === null || b === null) {
+    return 0; // Handle null values
+  }
+  return a + b;
+}
+
+console.log(foo(1, 2)); // Output: 3
+console.log(foo(null, 2)); // Output: 0
+console.log(foo(undefined, 2)); // Output: NaN Unexpected!
